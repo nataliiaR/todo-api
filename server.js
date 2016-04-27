f74fbff245d7583dc05f31b43f146b4d5ceca186
nataliiaR <nataliya.ryzha@gmail.com>
2016-04-27 11:48:24 -0400

    add auth token generation to Post/ users /login

--- server.js
+++ server.js
@@ -234,7 +234,12 @@ app.post('/users/login', function(req, res){
 	var body = _.pick(req.body, 'email','password');
 	// login our new class method in user model
 	db.user.login(body).then(function(user){
-		res.json(user.toPublicJSON());
+		var token = user.generateToken('authentication');
+		if (token){
+		res.header('Auth', token).json(user.toPublicJSON());
+	} else {
+		res.status(401).send();
+	}
 	}, function(e){
 		res.status(401).send();
 	});
