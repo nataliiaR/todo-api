ba9dd48d6ae49be0818b02962c7d91dbd9055ebd
nataliiaR <nataliya.ryzha@gmail.com>
2016-05-02 14:10:48 -0400

    added DELETE /users/login

--- server.js
+++ server.js
@@ -428,6 +428,17 @@ app.post('/users/login', function (req, res) {
 	});
 });
 
+
+//Delete /user/login
+
+app.delete('/users/login', middleware.requireAuthentication, function(req,res){
+	req.token.destroy().then(function(){
+		res.status(204).send();
+	}).catch(function(){
+		res.status(500).send();
+	})
+});
+
 db.sequelize.sync({force: true}).then(function() {
 	app.listen(PORT, function() {
 		console.log('Express listening on port ' + PORT + '!');
